ea562022d042ab43a7674e627f68a2c96ab2731e
pakispride1233333 <atmawahyudi57@gmail.com>
2026-04-11 05:22:38 +0700

    522,523

--- Minggu5/SortingMain01.java
+++ Minggu5/SortingMain01.java
@@ -2,7 +2,7 @@ package Minggu5;
 
 public class SortingMain01 {
     public static void main(String[] args) {
-        
+
         int a[] = {20, 10, 2, 7, 12};
 
         Sorting01 dataurut1 = new Sorting01(a, a.length);
@@ -10,9 +10,27 @@ public class SortingMain01 {
         System.out.println("Data awal:");
         dataurut1.tampil();
 
+        
         dataurut1.bubbleSort();
-
-        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC):");
+        System.out.println("Setelah BUBBLE SORT:");
         dataurut1.tampil();
+
+        
+        int b[] = {20, 10, 2, 7, 12};
+        Sorting01 dataurut2 = new Sorting01(b, b.length);
+
+        
+        dataurut2.selectionSort();
+        System.out.println("Setelah SELECTION SORT:");
+        dataurut2.tampil();
+
+        
+        int c[] = {20, 10, 2, 7, 12};
+        Sorting01 dataurut3 = new Sorting01(c, c.length);
+
+        
+        dataurut3.insertionSort();
+        System.out.println("Setelah INSERTION SORT:");
+        dataurut3.tampil();
     }
 }
